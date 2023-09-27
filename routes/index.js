6ebdadef80e36fde97c90d697a1bf20b5a8a1385
Zimuzocentennial <zimuzosamchikwe@gmail.com>
2023-09-27 14:42:28 +0100

    Add major Routes and Views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Chizimuzo Etolue Portfolio" });
-});
+const homeRouter = require("./home");
+const aboutRouter = require("./about");
+const projectRouter = require("./project");
+const serviceRouter = require("./service");
+const contactRouter = require("./contact");
+
+router.use("/", homeRouter);
+router.use("/about", aboutRouter);
+router.use("/projects", projectRouter);
+router.use("/services", serviceRouter);
+router.use("/contact", contactRouter);
 
 module.exports = router;
---
+++ routes/service.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("pageView", {
+    pageTitle: "Services",
+    paragraph: `Services ipsum dolor sit, amet consectetur adipisicing elit. Officia
+    necessitatibus, sequi pariatur accusamus fugit hic placeat. Odit numquam
+    quaerat maxime nobis corrupti ullam. Doloremque modi fugit ex qui,
+    perspiciatis odit quae maxime eveniet id sed.`,
+  });
+});
+
+module.exports = router;
