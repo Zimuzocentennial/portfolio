380f8a53ad0d69e33e6cd6902baa1935ac9449e8
Zimuzocentennial <zimuzosamchikwe@gmail.com>
2023-09-28 09:11:14 +0100

    Remove Dummy Texts from Views

--- routes/project.js
+++ routes/project.js
@@ -15,10 +15,10 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.render("pageView", {
     pageTitle: "Projects",
-    paragraph: `Projects ipsum dolor sit, amet consectetur adipisicing elit. Officia
-    necessitatibus, sequi pariatur accusamus fugit hic placeat. Odit numquam
-    quaerat maxime nobis corrupti ullam. Doloremque modi fugit ex qui,
-    perspiciatis odit quae maxime eveniet id sed.`,
+    paragraph: `As a web developer, I have experience working with JavaScript,
+    NodeJS, ExpressJS, EJS, HTML and CSS. I solely develop most of my projects and this
+    website is one of them. I usually complete the software development lifecycle from
+    design to release.`,
   });
 });
 
